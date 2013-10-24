5bea1f1d37ad75e68fd25f537a4bfacb81fa2588
Nicholas DiMucci <ndimucci84@gmail.com>
2013-10-24 09:16:02 -0400

    * Added Maven pom.xml for dependencies, thus removed lib folder * Added example usage of CacheBuilder in the message services.

--- src/com/madballneek/github/mvpboostrap/service/message/RudeMessageServiceImpl.java
+++ src/com/madballneek/github/mvpboostrap/service/message/RudeMessageServiceImpl.java
@@ -1,31 +1,60 @@
 package com.madballneek.github.mvpboostrap.service.message;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import com.madballneek.github.mvpboostrap.model.request.MessageRequestData;
 import com.madballneek.github.mvpboostrap.model.response.MessageResponseData;
+
+import java.util.concurrent.ExecutionException;
 
 /**
  * A, somewhat rude, <code>MessageService</code> implementation.
  */
 public class RudeMessageServiceImpl implements MessageService {
+	private LoadingCache<String, String> rudeHelloMessages;
 
 	public RudeMessageServiceImpl() {
+		rudeHelloMessages = CacheBuilder.newBuilder()
+				.maximumSize(10)
+				.build(new CacheLoader<String, String>() {
+					@Override
+					public String load(String key) throws Exception {
+						// We are just demonstrating a long running, expensive service process.
+						// Since we are running our service tasks on background threads, this won't
+						// hang up the application at all. You may execute other tasks to this service while this one runs!
+						System.out.println("Loading cache value...");
+						return createRudeHelloMessage(key);
+					}
+				});
 	}
 
 	@Override
 	public MessageResponseData sayHello(MessageRequestData requestData) {
-		// We are just demonstrating a long running service process.
-		// Since we are running our service tasks on background threads, this won't
-		// hang up the application at all. You may execute other tasks to this service while this one runs!
+		MessageResponseData responseData;
+
 		try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
+			// Performing a get on a CacheBuilder will also automatically put the value if it's not already present.
+			responseData = new MessageResponseData(rudeHelloMessages.get(requestData.name));
+		} catch (ExecutionException e) {
 			e.printStackTrace();
+			// In a real app, you'll want to handle exceptions a little bit better than this.
+			responseData = new MessageResponseData("");
 		}
-		return new MessageResponseData("What the hell do you want, " + requestData.name + "?");
+
+		return responseData;
 	}
 
 	@Override
 	public MessageResponseData sayGoodbye(MessageRequestData requestData) {
+		// Humor me once more, saying goodbye isn't expensive (though, it's always the hardest),
+		// so we don't need a CacheBuilder.
 		return new MessageResponseData("Don't let the door hit you, " + requestData.name);
+	}
+
+	private String createRudeHelloMessage(String name) throws InterruptedException {
+		// Again, just simulating a long running process.
+		Thread.sleep(5000);
+		return "What the hell do you want, " + name + "?";
 	}
 }
--- src/com/madballneek/github/mvpboostrap/service/message/SimpleMessageServiceImpl.java
+++ src/com/madballneek/github/mvpboostrap/service/message/SimpleMessageServiceImpl.java
@@ -1,31 +1,63 @@
 package com.madballneek.github.mvpboostrap.service.message;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import com.madballneek.github.mvpboostrap.model.request.MessageRequestData;
 import com.madballneek.github.mvpboostrap.model.response.MessageResponseData;
+
+import java.util.concurrent.ExecutionException;
 
 /**
  * MessageService implementation that simply replies back to the client with a hello or goodbye message.
  */
 public class SimpleMessageServiceImpl implements MessageService {
+	private LoadingCache<String, String> helloMessages;
 
 	public SimpleMessageServiceImpl() {
+		// Let's pretend this service is expensive to execute (although in this example, it's not).
+		// Taking advantage of CacheBuilder will allow us to cache expensive results and return them immediately
+		// if they are ever requested again.
+		helloMessages = CacheBuilder.newBuilder()
+				.maximumSize(10)
+				.build(new CacheLoader<String, String>() {
+					@Override
+					public String load(String key) throws Exception {
+						// We are just demonstrating a long running, expensive service process.
+						// Since we are running our service tasks on background threads, this won't
+						// hang up the application at all. You may execute other tasks to this service while this one runs!
+						System.out.println("Loading cache value...");
+						return createHelloMessage(key);
+					}
+				});
 	}
 
 	@Override
-	public MessageResponseData sayHello(MessageRequestData requestData) {
-		// We are just demonstrating a long running service process.
-		// Since we are running our service tasks on background threads, this won't
-		// hang up the application at all. You may execute other tasks to this service while this one runs!
+	public MessageResponseData sayHello(final MessageRequestData requestData) {
+		MessageResponseData responseData;
+
 		try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
+			// Performing a get on a CacheBuilder will also automatically put the value if it's not already present.
+			responseData = new MessageResponseData(helloMessages.get(requestData.name));
+		} catch (ExecutionException e) {
 			e.printStackTrace();
+			// In a real app, you'll want to handle exceptions a little bit better than this.
+			responseData = new MessageResponseData("");
 		}
-		return new MessageResponseData("Hey there, " + requestData.name);
+
+		return responseData;
 	}
 
 	@Override
 	public MessageResponseData sayGoodbye(MessageRequestData requestData) {
+		// Humor me once more, saying goodbye isn't expensive (though, it's always the hardest),
+		// so we don't need a CacheBuilder.
 		return new MessageResponseData("See yea, " + requestData.name);
+	}
+
+	private String createHelloMessage(String name) throws InterruptedException {
+		// Again, just simulating a long running process.
+		Thread.sleep(5000);
+		return "Hey there, " + name;
 	}
 }
